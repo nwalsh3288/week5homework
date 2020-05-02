baf0b908425d2da8f6e37c821d5214eaf369d3d9
Nicole Walsh <nmwalsh92@gmail.com>
2020-05-02 16:59:33 -0700

    draft 2

--- Develop/javahw.js
+++ Develop/javahw.js
@@ -16,66 +16,96 @@ $(document).ready(function(){
         // logic .onclick and save/grab what user had entered
 
         $(".save1").on("click",function() {
+            // capture textbox content by moving through the DOM from save1 to associated textbox
+            // setting varrible to texted entered in textbox
+            var textEnteredIntoField = $(this)[0].parentNode.children[1].children[0].children[0].value
+            console.log(textEnteredIntoField);
+            // setting varrible to html "save" attribute
+            var elementBeingClickedOn = $(this).attr("data-time");
+            console.log(elementBeingClickedOn);
+            localStorage.setItem(elementBeingClickedOn, JSON.stringify(textEnteredIntoField));
+           
+            var retrieveText = localStorage.getItem(elementBeingClickedOn);
+            var parse = JSON.parse(retrieveText)
+            console.log(parse);
 
-        // GRAB TEXT    
-        //    capture textbox content by moving through the DOM from save1 to associated textbox
-        var textEnteredIntoField = $(this)[0].parentNode.children[1].children[0].children[0].value
-            // path goes from the save button to the text area
-
-        // GRAB TEXTBOX ID (that we named one, two, three, etc) - know which hour the text coming from
-        //  saving to the back end and organizing it so that it displays in the right section on the screen
-        // identifying the ID that we need to save this note for - how we are going to populate page where we saved it
-        var idFromHTMLRepresentingHour = $(this)[0].parentNode.children[1].children[0].children[0].id
-        console.log(idFromHTMLRepresentingHour);
+            var display 
+        });
+    
+    
+});
 
 
-        // SAVE TO BACKEND
-        // now have to populate jsonNotes correctly
-        jsonToStoreLocallyForHours.idfromHTMLRepresentingHour = textEnteredIntoField;
+//         // GRAB TEXT    
+//         //    capture textbox content by moving through the DOM from save1 to associated textbox
+//         var textEnteredIntoField = $(this)[0].parentNode.children[1].children[0].children[0].value
+//         var elementBeingClickedOn = $(this).attr("data-time");
+//         console.log(textEnteredIntoField);
+//         console.log(elementBeingClickedOn);
+
+//             // path goes from the save button to the text area
+
+//         // GRAB TEXTBOX ID (that we named one, two, three, etc) - know which hour the text coming from
+//         //  saving to the back end and organizing it so that it displays in the right section on the screen
+//         // identifying the ID that we need to save this note for - how we are going to populate page where we saved it
+//         var idFromHTMLRepresentingHour = $(this)[0].parentNode.children[1].children[0].children[0].id
+//         console.log(idFromHTMLRepresentingHour);
 
 
-        // Show on the front end
+//         // setting textboxID ID to user entered text
+//         idfromHTMLRepresentingHour = textEnteredIntoField;
+
+//         localStorage.setItem(elementBeingClickedOn, JSON.stringify(textEnteredIntoField));
+
+//         // local storage --> getitem function --> put the text area -- entering in those values there
+//         localStorage.getItem("one oclock");
+        
+//         });
+//         // var storedTodos = JSON.parse(localStorage.getItem("todos"));
+
+       
 
 
-        localStorage.setItem("userEntry", JSON.stringify(jsonToStoreLocallyForHours));
+//     //   key that represents every hour - key: value (getting saved to local storage)
+//     // JSON PARSE?
 
-        console.log(textEnteredIntoField);
-          });    
+//     // data name (property): 1
+//     // need append - set value of object
+//     // jasonToStoreLocallyForHours.one = textEnterIntoField -- after oject is declared
 
-    //   key that represents every hour - key: value (getting saved to local storage)
-    var jsonToStoreLocallyForHours = {
-        one: "",
-        two: "",
-        three: "",
-        four: "",
-        five: "",
-        six: "",
-        seven: "",
-        eight: "",
-        nine: "",
-        ten: "",
-        eleven: "",
-        twelve: ""
-    }
+//     // var jsonToStoreLocallyForHours = {
+//     //     one: "",
+//     //     two: "",
+//     //     three: "",
+//     //     four: "",
+//     //     five: "",
+//     //     six: "",
+//     //     seven: "",
+//     //     eight: "",
+//     //     nine: "",
+//     //     ten: "",
+//     //     eleven: "",
+//     //     twelve: ""
+//     // }
 
 
         
 
 
-    // THEN the text for that event is saved in local storage
-    // THEN the saved events persist
-    // logic grab what user had entered, localstorage.get item
+//     // // THEN the text for that event is saved in local storage
+//     // THEN the saved events persist
+//     // logic grab what user had entered, localstorage.get item
 
-});
+// });
 
-// raw HTML page
-// jQuery
-// Logic
+// // raw HTML page
+// // jQuery
+// // Logic
 
 
-// THEN I am presented with timeblocks for standard business hours
-    // jQuery
-// WHEN I view the timeblocks for that day
+// // THEN I am presented with timeblocks for standard business hours
+//     // jQuery
+// // WHEN I view the timeblocks for that day
 // WHEN I click into a timeblock
     // Logic .onclick
 // THEN I can enter an event
@@ -89,8 +119,3 @@ $(document).ready(function(){
 // ```
 
 // let root = document.documentElement;
-
-// root.addEventListener("mousemove", e => {
-//     root.style.setProperty('--mouse-x', e.clientX + "px");
-//     root.style.setProperty('--mouse-y', e.clientY + "px");
-//   });
